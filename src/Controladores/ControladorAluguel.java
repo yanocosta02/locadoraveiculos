c5b12ee9ac7d160e772b204c1fdead9b5ae64d92
Yan Costa <104322543+yanocosta02@users.noreply.github.com>
2023-11-26 21:56:24 -0300

    Cadastrando Automovel e mostrando

---
+++ src/Controladores/ControladorAluguel.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controladores;
+
+import java.util.ArrayList;
+import Modelo.Aluguel;
+
+/**
+ *
+ * @author Yan
+ */
+public class ControladorAluguel {
+    ArrayList <Aluguel> listaAlugueis = new ArrayList<>();
+    
+    public void cadastrar(Aluguel a) {
+	listaAlugueis.add(a);
+    }
+    
+    public ArrayList <Aluguel> listarClientes() {
+        return listaAlugueis;
+    }    
+}
---
+++ src/Controladores/ControladorCliente.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controladores;
+
+import java.util.ArrayList;
+import Modelo.Cliente;
+
+/**
+ *
+ * @author Yan
+ */
+public class ControladorCliente {
+    private static ControladorCliente instance;
+    ArrayList<Cliente> listaClientes = new ArrayList<>();
+    
+    
+    //padrão singleton para ficar a mesma classe para guardar as informações
+    public static ControladorCliente getInstance() {
+        if (instance == null) {
+            instance = new ControladorCliente();
+            // Inicialize outros atributos, se necessário
+        }
+        return instance;
+    }
+    
+    public void Cadastrar(Cliente c) {
+        if (!existeCliente(c.getCpf())) {
+            listaClientes.add(c);
+        } else {
+            System.out.println("Cliente já cadastrado.");
+        }
+    }
+
+    public ArrayList<Cliente> listarClientes() {
+        return listaClientes;
+    }
+
+    public Cliente buscaCliente(int cpf) {
+        for (Cliente cliente : listaClientes) {
+            if (cliente.getCpf() == cpf) {
+                return cliente;
+            }
+        }
+        return null; // Se não encontrar o cliente com o CPF especificado
+    }
+
+    public boolean existeCliente(int cpf) {
+        return buscaCliente(cpf) != null;
+    }
+}
